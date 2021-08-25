2d547a2a0f348d5b40f5c75f58136fe9a0454d0d
Crankobil <84187431+Crankobil@users.noreply.github.com>
2021-08-25 04:36:39 +0100

    Included line clamp

--- m4d2-homework/src/Components/LatestRelease.jsx
+++ m4d2-homework/src/Components/LatestRelease.jsx
@@ -6,17 +6,14 @@ export default class BookList extends Component {
   render() {
     return (
       <Container>
-        <Row className="row-cols-sm-1 row-cols-md-3 row-cols-lg-4 my-4">
+        <Row className="row-cols-sm-1 row-cols-md-3 row-cols-lg-4">
           {books.map((book) => (
-            <Col>
+            <Col mb-3>
               <Card h-100>
                 <Card.Img variant="top" src={book.img}/>
                 <Card.Body>
-                  <Card.Title>{book.title}</Card.Title>
-                  <Card.Text>
-                    This is a "MUST READ"
-                  </Card.Text>
-                  <Button variant="secondary">${book.price}</Button>
+                  <Card.Title className="line-clamp">{book.title}</Card.Title>
+                    <Button mt-auto variant="secondary">${book.price}</Button>
                 </Card.Body>
               </Card>
             </Col>
